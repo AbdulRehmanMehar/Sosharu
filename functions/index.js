a36a95e4e2e8d5930948e7296941b9d4e75b6c11
Abdul Rehman <mehars.6925@gmail.com>
2018-09-26 15:35:16 +0500

    + Pug for dynamic views

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('views', __dirname + '/resources/views');
+app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
-  res.send('It Works!');
+  res.render('index');
 });
 
 exports.app = functions.https.onRequest(app);
