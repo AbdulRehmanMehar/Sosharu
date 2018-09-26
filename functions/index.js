b8d6e64ad7d34282365d22862adfe154058ef393
Abdul Rehman <mehars.6925@gmail.com>
2018-09-26 16:07:36 +0500

    added controller

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,12 @@ const functions = require('firebase-functions');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mainController = require('./controllers/main');
 
 app.set('views', __dirname + '/resources/views');
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-app.get('/', (req,res) => {
-  res.render('index');
-});
+app.use('/', mainController);
 
 exports.app = functions.https.onRequest(app);
