147c07aa42d01ef4179d55f1145f7ff1bb0fe9a6
Abdul Rehman <mehars.6925@gmail.com>
2018-09-27 13:48:46 +0500

    submit forms silently

--- functions/resources/assets/js/app.js
+++ functions/resources/assets/js/app.js
@@ -1,2 +1,19 @@
-require('./partial');
-console.log('Gulp Rocks!');
+$(document).ready(() => {
+
+  // Submit Forms Silently.
+  $('#silent-form').submit((e) => {
+    e.preventDefault();
+    $.ajax({
+      type: $(e.target).attr('method'),
+      url: $(e.target).attr('action'),
+      data: $(e.target).serialize(),
+      success: (data) => {
+        console.log(data);
+      },
+      error: (data) => {
+        console.log(data);
+      }
+    });
+  });
+
+});
