d05a1b124b55a075397443df09d18b0952d7f538
Abdul Rehman <mehars.6925@gmail.com>
2018-10-01 15:29:02 +0500

    firebase admin configration

--- functions/config/firebase-init.js
+++ functions/config/firebase-init.js
@@ -1,9 +1,13 @@
+const functions = require('firebase-functions');
 const admin = require("firebase-admin");
+
 const serviceAccount = require("./firebase.json");
 
 const firebase = admin.initializeApp({
   serviceAccount: serviceAccount,
   databaseURL: "https://social-57b13.firebaseio.com"
 });
+
+// const firebase = admin.initializeApp(functions.config().firebase);
 
 module.exports = firebase;
