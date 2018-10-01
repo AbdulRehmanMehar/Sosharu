4798cba1c071c03a5224e09b4ff0ddf8703ad0ae
Abdul Rehman <mehars.6925@gmail.com>
2018-10-01 15:30:32 +0500

    + error handling

--- functions/resources/assets/js/app.js
+++ functions/resources/assets/js/app.js
@@ -11,7 +11,25 @@ $(document).ready(() => {
         console.log(data);
       },
       error: (data) => {
-        console.log(data);
+        let errors = data.responseJSON.errors;
+        console.log(errors);
+        $(errors).each((i,{param, msg}) => {
+          // Removing Old Messages
+          $(e.target).children('input').removeClass('is-invalid');
+          $(e.target).children('small.invalid-feedback').html('');
+          // Setting New Messages
+          $(`[name=${param}]`).addClass('is-invalid');
+          $(`[name=${param}]`).next().html(msg);
+
+          // Check for universal errors
+          if(param == "error"){
+            let error = $(`<div class="alert alert-danger" role="alert"><b>${msg}</b></div>`);
+            $(".alert-saved").append(error);
+            setTimeout(function () {
+              $(error).remove();
+            }, 2000);
+          }
+        });
       }
     });
   });
