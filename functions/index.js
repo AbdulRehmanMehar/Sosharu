985c3e9bf635cb208c9eaaa29b7bad3f2a35157e
Abdul Rehman <mehars.6925@gmail.com>
2018-10-01 15:42:35 +0500

    commented the code

--- functions/index.js
+++ functions/index.js
@@ -10,29 +10,38 @@ const mainController = require('./controllers/main');
 const csrfHandler = require('./middlewares/csrf-handler');
 
 
-app.set('views', __dirname + '/resources/views');
-app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({extended: true}));
-app.disable('x-powered-by');
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(cookieParser());
-app.use(helmet());
-app.use(compression());
-app.use(csrf({ cookie: true }));
-// app.use(csrfHandler);
+// ------------------------------------------------------
+//                     Views Setup
+// ------------------------------------------------------
 
-// app.use((req, res, next) => {
-//   res.locals._csrf = req.csrfToken();
-//   next();
-// });
-// app.use((err, req, res, next) => {
-//   if (err.code !== 'EBADCSRFTOKEN') return next(err);
-//   res.status(403).json({
-//     param: "error",
-//     msg: "Invalid CSRF Token"
-//   });
-// });
+app.set('views', __dirname + '/resources/views'); 
+app.set('view engine', 'pug');
+
+// ------------------------------------------------------
+//                     Express Modules
+// ------------------------------------------------------
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(express.json());
+app.use(cookieParser()); 
+app.use(compression()); // makes app faster
+
+// ------------------------------------------------------
+//                     Securing the App
+// ------------------------------------------------------
+
+app.disable('x-powered-by');
+app.use(helmet());
+app.use(csrf({ cookie: true })); // csrf protection
+app.use(csrfHandler); // handler for csrf
+
+// ------------------------------------------------------
+//                     Controllers
+// ------------------------------------------------------
+
 app.use('/', mainController);
+
+
 
 exports.app = functions.https.onRequest(app);
