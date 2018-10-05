0b5c34aac3fe7f1798e1d2d7b4dd48e2941d065c
Abdul Rehman <mehars.6925@gmail.com>
2018-10-05 12:22:56 +0500

    dynamic credentials

--- functions/config/firebase-init.js
+++ functions/config/firebase-init.js
@@ -1,13 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase.json");
-
-const firebase = admin.initializeApp({
-  serviceAccount: serviceAccount,
-  databaseURL: "https://social-57b13.firebaseio.com"
-});
-
-// const firebase = admin.initializeApp(functions.config().firebase);
+const firebase = admin.initializeApp(functions.config().firebase);
 
 module.exports = firebase;
