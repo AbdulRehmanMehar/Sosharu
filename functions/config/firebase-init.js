2ad7fc4593e0f67530e6d972cbb54223348a96cf
Abdul Rehman <mehars.6925@gmail.com>
2018-10-11 14:04:37 +0500

    added service account keys

--- functions/config/firebase-init.js
+++ functions/config/firebase-init.js
@@ -1,6 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 
-const firebase = admin.initializeApp(functions.config().firebase);
+const serviceAccount = require("./social-57b13-firebase-adminsdk-shwlp-c694d33803.json");
+
+
+// const firebase = admin.initializeApp(functions.config().firebase);
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://social-57b13.firebaseio.com"
+});
 
 module.exports = firebase;
