1299ccad8ce7018fe89bd0291fdd1b900b8ace2d
Abdul Rehman <mehar@pop-os.localdomain>
2018-10-13 22:54:57 +0500

    added loader

--- functions/resources/assets/js/app.js
+++ functions/resources/assets/js/app.js
@@ -1,14 +1,33 @@
 $(document).ready(() => {
 
+
+  let loader = (param) => {
+    // takes param, start or stop
+    if (param == "start") {
+      $('body').append(`
+        <div class="loader">
+          <h1 class="brand"><span>So</span>sharu</h1>
+          <h5>your request is being processed</h5>
+          <div class="prog">
+            <div class="res"></div>
+          </div>
+        </div>
+      `);
+    }
+    if (param == "stop") {
+      $('.loader').remove();
+    }
+  };
+
   // Submit Forms Silently.
   $('.silent-form').each((i, element) => {
     $(element).submit((e) => {
       e.preventDefault();
-
+      loader('start');
       $('input.is-invalid').each((e, input) => {
         $(input).removeClass('is-invalid');
       });
-      
+
       $('span.is-invalid').each((e, span) => {
         $(span).html('');
       });
@@ -18,9 +37,11 @@ $(document).ready(() => {
         url: $(e.target).attr('action'),
         data: $(e.target).serialize(),
         success: (data) => {
+          loader('stop');
           console.log(data);
         },
         error: (data) => {
+          loader('stop');
           let errors = data.responseJSON.errors;
           console.log(errors);
           $(errors).each((i, {
@@ -29,11 +50,11 @@ $(document).ready(() => {
           }) => {
             // Setting New Messages
             $(`[name=${param}]`).addClass('is-invalid');
-            $(`[name=${param}]`).next().html(msg);
+            $(`[name=${param}]`).next().html(msg.replace('Error: ', ''));
 
             // Check for universal errors
             if (param == "error") {
-              let error = $(`<div class="alert alert-danger" role="alert"><b>${msg}</b></div>`);
+              let error = $(`<div class="alert alert-danger" role="alert"><b>${msg.replace('Error: ', '')}</b></div>`);
               $(".alert-saved").append(error);
               setTimeout(function () {
                 $(error).remove();
