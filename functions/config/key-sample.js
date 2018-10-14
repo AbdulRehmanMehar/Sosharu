da07f6533a1ea8a226b0aef0834dec1d8632f127
Abdul Rehman <mehar@pop-os.localdomain>
2018-10-14 10:01:12 +0500

    config sample

---
+++ functions/config/key-sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+  facebook: {
+    clientID: "",
+    clientSecret: "",
+    callbackURL: "https://social-57b13.firebaseapp.com/auth/facebook/redirect",
+  },
+  google: {
+    clientID: "",
+    clientSecret: "",
+    callbackURL: "https://social-57b13.firebaseapp.com/auth/google/redirect"
+  },
+  encKey: "", // Encryption Key for Sessions
+  nodemailer: {
+    api: "" // SendGrid API
+  }
+};
